f54543198eeaab895a0b332a8720675abe869871
MonicaGrages <mgrages1@gmail.com>
2017-06-30 12:03:08 -0400

    artist controller methods working with postman

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  # We could use JSON web tokens here too, which is a popular option
+  protect_from_forgery unless: -> { request.format.json? }
 end
---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,61 @@
+class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+    render json: @artists
+    #Overwrites the assumption to render an index.html.erb
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      #we don't want the bang after save because if we have it, we won't get the JSON error below, which we need to give to angular so we can render an error to the user
+      redirect_to artist_path(@artist)
+    else
+      render status: 500,
+        json: {
+        error: @artist.errors
+      }
+    end
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+    @songs = @artist.songs
+    render json: {
+      "artist": @artist,
+      "songs": @songs
+    }
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    # @artist.update(artist_params)
+    if @artist.update(artist_params)
+      #we don't want the bang after save because if we have it, we won't get the JSON error below, which we need to give to angular so we can render an error to the user
+      redirect_to artist_path(@artist)
+    else
+      render status: 500,
+        json: {
+        error: @artist.errors
+      }
+    end
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    if @artist.destroy
+      redirect_to artists_path
+    else
+      render status: 500,
+        json: {
+          error: "Could not delete Artist"
+        }
+    end
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :photo_url, :nationality)
+  end
+end
